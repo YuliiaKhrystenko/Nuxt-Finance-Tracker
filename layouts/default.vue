94f570c79655dab0431719add4e23d7b77da4785
Yuliia <yu6785963@gmail.com>
2024-06-10 11:03:30 +0300

    Project structure

---
+++ layouts/default.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container mx-auto max-w-4xl">
+        <AppHeader />
+        <main>
+            <slot />
+        </main>
+    </div>
+</template>
+
+<script setup>
+useHead({
+    link: [
+        {
+            rel: 'preconnect',
+            href: 'https://fonts.googleapis.com'
+        },
+        {
+            rel: 'stylesheet',
+            href: 'https://fonts.googleapis.com/css2?family=Montserrat&display=swap',
+            crossorigin: ''
+        }
+    ]
+})
+</script>
+
+<style>
+body {
+    font-family: "Montserrat", sans-serif;
+}
+
+body {
+    @apply bg-white dark:bg-gray-800 text-gray-800 dark:text-white
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>Hello index page!</div>
+</template>
