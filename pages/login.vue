77968f45451de68655d6b725ec6a4ff68473ca17
Yuliia <yu6785963@gmail.com>
2024-07-17 17:02:00 +0300

    add runtimeConfig

--- pages/login.vue
+++ pages/login.vue
@@ -36,6 +36,7 @@ const email = ref('')
 const pending = ref(false)
 const { toastError } = useAppToast()
 const supabase = useSupabaseClient()
+const redirectUrl = useRuntimeConfig().public.baseUrl
 
 useRedirectIfAuthenticated()
 
@@ -46,7 +47,7 @@ const handleLogin = async () => {
         const { error } = await supabase.auth.signInWithOtp({
             email: email.value,
             options: {
-                emailRedirectTo: 'http://localhost:3000/confirm'
+                emailRedirectTo: `${redirectUrl}/confirm`
             }
         })
 
