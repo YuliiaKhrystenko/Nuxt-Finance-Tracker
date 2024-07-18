d9db44b3ad9417958699e0acc3ad7caf53ea4b87
Yuliia <yu6785963@gmail.com>
2024-07-18 18:48:08 +0300

    edit confirm page

--- pages/confirm.vue
+++ pages/confirm.vue
@@ -9,33 +9,4 @@
 
 <script setup>
 useRedirectIfAuthenticated()
-
-
-import { useRoute } from 'vue-router';
-import { useSupabaseClient } from '@supabase/supabase-js';
-
-const route = useRoute();
-const supabase = useSupabaseClient();
-
-const confirmSignUp = async () => {
-    const { access_token } = route.query;
-    if (access_token) {
-        const { error } = await supabase.auth.signIn({
-            access_token
-        });
-
-        if (error) {
-            // Handle error (e.g., show a message to the user)
-            console.error("Error confirming sign-up:", error);
-        } else {
-            // Redirect to a success page or dashboard
-            window.location.href = '/dashboard';
-        }
-    } else {
-        // Handle missing access token
-        console.error("No access token provided in URL");
-    }
-};
-
-confirmSignUp();
 </script>
